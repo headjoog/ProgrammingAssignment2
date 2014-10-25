59ab644b0d330e9ccddb34c400d1fd7fb89e01cf
muttinthehut <JoeB615@gmail.com>
2014-10-25 08:02:09 -0500

    add functions to cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -5,23 +5,28 @@
 
 ## makeCacheMatrix:  creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+    # variable "m" stores the matrix
     m <- NULL
-    #  get: get the value of the matrix
+    
+    #  get: return the value of the matrix (in the current environment)
     get <- function() x
     
-    # set:  set the value of the matrix
+    # set:  set the value of the matrix to the parameter
+    #       and copy to variable "x".  
     set <- function(y) {
       x <<- y
       m <<- NULL
     }
     
-    # setinverse: set the inverse of the matrix
+    # setinverse: set the inverse of the matrix using "solve" function.
+    #             Note the "superassignment" to m, which is found in 
+    #             calling environments.
     setinverse <- function(solve) m <<- solve
     
     # getinverse: get the inverse of the matrix
     getinverse <- function() m
     
-    # return the results
+    # construct a list of functions that have been defined
     list(set = set, get = get
         ,setinverse=setinverse
         ,getinverse=getinverse)    
@@ -37,16 +42,24 @@ cacheSolve <- function(x, ...) {
        ## get the inverse of the matrix
        m <- x$getinverse()
        
-       ## check if there's alreay a matrix
+       ## check if there's alreay a matrix.  If it's defined
+       ## return the cached value (let the user know it's cached)
+       ## and exit function
       if (!is.null(m)) {
         message("grabbing data from the cache")
         return(m)
       }
       
-      ## if not:  get the inverse of the matrix
+      ## if we haven't exited function (from using cached version above)
+      ## assign the matrix to a variable...
       data <- x$get()
+      
+      ## and use the solve function to get the inverse
       m <- solve(data, ...)
+      
       ## set the inverse of the matrix
       x$setinverse(m)
+      
+      ## and return value
       m
 }
